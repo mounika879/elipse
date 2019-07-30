3210d3b0972d4883181486e06415eec336ede310
mounika <kodatimounika@gmail.com>
2019-07-30 14:36:34 +0530

    this is sample26

---
+++ src/test/java/simpe26/simpletest.java
@@ -0,0 +1,14 @@
+package simpe26;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class simpletest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
